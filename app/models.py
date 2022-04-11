4769315eb414a77762fc3a1c080d20664564fe7a
cheran <cherankumar.548@gmail.com>
2022-04-11 18:06:34 +0530

    first model created and working without img

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Record(models.Model):
+    name = models.CharField(max_length=100, null=True, blank=True)
+    species = models.CharField(max_length=100, null=True, blank=True)
+    weight = models.IntegerField(null=True, blank=True)
+    length = models.IntegerField(null=True, blank=True)
+    latitude = models.FloatField(null=True, blank=True)
+    longitude = models.FloatField(null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
--- app/views.py
+++ app/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import *
 # Create your views here.
 
-def sample(request):
-    return HttpResponse("hello")
-
+@api_view(['POST'])
 def createRecord(request):
-    return HttpResponse("working")
+    data = request.data
+    try:
+        record = Record.objects.create(
+            name=data.get('name'),
+            species=data.get('species'),
+            weight=data.get('weight'),
+            length=data.get('length'),
+            latitude=data.get('latitude'),
+            longitude=data.get('longitude'),
+        )
+
+        record.save()
+        return Response({"message":"record added successfully"}, status=status.HTTP_201_CREATED)
+    except:
+        return Response({"message":"error in record u added"}, status=status.HTTP_400_BAD_REQUEST)
+
